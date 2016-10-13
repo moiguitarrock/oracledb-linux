044d1d8099a6b8a7563264d738ea740c5e238cb6
Moises Cuervo <maccma.c2@gmail.com>
2016-10-13 16:51:30 -0500

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: process.env.NODE_ORACLEDB_USER || "HEALTHQA",
-  password: process.env.NODE_ORACLEDB_PASSWORD || "HEALTHQA",
-  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.21.100.197:1521/PDB_CEBTEST01",
+  user: process.env.NODE_ORACLEDB_USER,
+  password: process.env.NODE_ORACLEDB_PASSWORD,
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING,
   externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
 };
